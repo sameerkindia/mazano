21efde8d6eda8fb9d33815325c57af4636fa969c
sameerExpansionjs <sameer@expansionjs.com>
2024-07-15 16:55:28 +0530

    home page mobile responsive

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -2,14 +2,14 @@ import Button from "../UI/Button";
 
 function Hero() {
   return (
-    <section className="min-h-[30vw] bg-primary-color flex items-center justify-center pt-[5vw]">
-      <div className="container flex items-center  ">
+    <section className="min-h-[50vw] bg-primary-color flex items-center justify-center pt-[10vw] md:pt-[5vw]">
+      <div className="container flex flex-col md:flex-row items-center  ">
         <div className="grow flex flex-col gap-[3vw]">
           <div className="opacity-70">
-            <h3 className="text-[1.8vw] tracking-wide ">
+            <h3 className="text-[3vw] md:text-[1.8vw] tracking-wide ">
               Let's solve hunger together with monthly Offer
             </h3>
-            <h2 className="flex flex-col text-[5.5vw] font-light uppercase  leading-tight">
+            <h2 className="flex flex-col text-[9vw] md:text-[5.5vw] font-light uppercase  leading-tight">
               <span>Organic</span>
               <span>pasta & sauce</span>
             </h2>
@@ -18,11 +18,11 @@ function Hero() {
             <Button href="/">SHOP NOW</Button>
           </div>
         </div>
-        <div className="basis-2/4">
+        <div className="basi s-2/4 ">
           <img
             src="images/hero-image.png"
             alt="italian pasta image"
-            className=""
+            className="w-[65vw] md:w-[40vw] mx-auto"
           />
         </div>
       </div>
--- components/UI/Button/index.jsx
+++ components/UI/Button/index.jsx
@@ -4,7 +4,7 @@ function Button({ children, href, className }) {
   return (
     <Link
       href={href}
-      className={`${className} bg-secondary-color inline-block text-white py-[1vw] px-[3vw] text-[1.4vw] `}
+      className={`${className} bg-secondary-color inline-block text-white py-[1.5vw] md:py-[1vw] px-[4vw] md:px-[3vw] text-[2vw] md:text-[1.4vw] `}
     >
       {children}
     </Link>
